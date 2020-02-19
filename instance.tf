f71aaa6c6ff7b0a94d99911cc34cf772f01d5e57
Noaman <moaaz.noaman@c02yf3edjgh6.wirecard.lan>
2020-02-19 10:36:47 +0100

    adding files

--- instance.tf
+++ instance.tf
@@ -16,18 +16,3 @@ resource "aws_instance" "example" {
     Name = "development_${count.index}"
   }
 }
-
-//resource "aws_ebs_volume" "ebs-volume-1" {
-//  availability_zone = "eu-west-1a"
-//  size              = 20
-//  type              = "gp2"
-//  tags = {
-//    Name = "extra volume data"
-//  }
-//}
-//
-//resource "aws_volume_attachment" "ebs-volume-1-attachment" {
-//  device_name = "/dev/xvdh"
-//  volume_id   = aws_ebs_volume.ebs-volume-1.id
-//  instance_id = aws_instance.example.id
-//}
